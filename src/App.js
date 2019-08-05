da497c51cda9ec326ec53c2606b7f5a9b6db4e07
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-05 15:33:08 -0400

    working on project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
+
+// const todoData = [
+//   {
+//     task: 'Organize Garage',
+//     id: 121,
+//     completed: false
+//   },
+//   {
+//     task: 'Bake Cookies',
+//     id: 122,
+//     completed: false
+//   },
+//   {
+//     task: 'Organize Art Room',
+//     id: 123,
+//     completed: false
+//   },
+//   {
+//     task: 'Meal Prep',
+//     id: 124,
+//     completed: false
+//   },
+//   {
+//     task: 'Grocery Shop',
+//     id: 125,
+//     completed: false
+//   },
+//   {
+//     task: 'Study JS',
+//     id: 126,
+//     completed: false
+//   }
+// ];
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      task: [],
+      id: Date.now(),
+      completed: false
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
